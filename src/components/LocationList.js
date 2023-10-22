e42bdcbf84e32781d8a721abdab989f55f7008d4
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-10-22 16:44:21 -0700

    refactored Location components

--- src/components/LocationList.js
+++ src/components/LocationList.js
@@ -5,6 +5,21 @@ import useAPI from '../useAPI'
 
 import '../styles.css';
 
+const LocationFormState = Object.freeze({
+    Edit: "Edit",
+    New: "New",
+});
+
+const LocationPanelState = Object.freeze({
+    Data: "Data",
+    Edit: "Edit",
+    New: "New",
+    AddButton: "AddButton"
+});
+
+
+const locationForm =  {address: "", info: ""}
+
 function LocationList({
     locations = [], 
     selectedLocationId = null, 
@@ -13,12 +28,7 @@ function LocationList({
   
     const { isStoreOwner } = useAuth();  
   
-    const [showNewLocationForm, setShowNewLocationForm] = useState(false);
-  
-    const {loading,deleteLocation, editLocation, postLocation  } = useAPI()
-  
-  
-    useEffect(()=>setShowNewLocationForm(false), [locations])
+    const {loading, deleteLocation, editLocation, postLocation  } = useAPI()
   
     function addLocation(new_location){
       postLocation({new_location},(result)=>{refetchLocations()})
@@ -31,114 +41,151 @@ function LocationList({
     function _editStoreOwnerLocation(location){
         editLocation({location},(result)=>{refetchLocations()})
     }
-  
-  //console.log(locations)
+
     return(<>
       {locations ? locations.map( (location, idx)=>
-          <LocationCard key={idx} location={location}//{...location} 
-            selectedLocationId={selectedLocationId} 
-            selectLocation={selectLocation}
-            deleteLocation={_deleteLocation}
-            editStoreOwnerLocation={_editStoreOwnerLocation}/>) 
+          <LocationPanel //LocationCard 
+            key={idx}
+            isLocationSelected = {location.id === selectedLocationId}
+            startingMode = {LocationPanelState.Data}
+            {...{location,selectLocation,_deleteLocation, _editStoreOwnerLocation}}/>) 
         : <></>}
-  
-       
-        {isStoreOwner() ? 
-          (showNewLocationForm ? 
-            <div className="location_card">
-              <LocationForm 
-                cancelLocationForm={e=>setShowNewLocationForm(false)} 
-                putStoreOwnerLocation={addLocation}/> 
-            </div> : 
-            <button onClick={e=>setShowNewLocationForm(true)}>Add New Location</button>)
-        : <></>}
-  
+
+        {isStoreOwner() ? <>
+            <LocationPanel   
+                {...{_deleteLocation,_editStoreOwnerLocation}}
+                putStoreOwnerLocation={addLocation}
+                selectedLocationId={selectedLocationId}
+                startingMode = {LocationPanelState.AddButton}/>
+        </> : <></> }
     </>)
-  }
-  
-  
-  function LocationCard({
-   // info, address, id, LatLng, 
-    location,
-    selectedLocationId, 
-    selectLocation=()=>{}, 
-    deleteLocation=()=>{}, 
-    editStoreOwnerLocation=()=>{}}){
-  
-    const { isStoreOwner } = useAuth();  
-  
-    const [isEdit, setIsEdit] = useState(false);
-  
-    useEffect(()=>setIsEdit(false), [location, selectedLocationId])
-  
-    const {info, address, id, LatLng, } = location
-  
-    const isLocationSelected = ()=> id===selectedLocationId
-  
-    return( 
-      <div className={id===selectedLocationId ? "location_card location_card_selected" : "location_card"  } 
-          onClick={ e=>{ selectLocation(id)}}>
-          
-  
-      { isEdit ? <>
-        <LocationForm 
-          cancelLocationForm={e=>setIsEdit(false)} 
-          putStoreOwnerLocation={editStoreOwnerLocation}
-          form={ {...location} }/></> 
-      :
-      <>
-        {isStoreOwner() && isLocationSelected() ? 
-          <div className="location_card_btns"> 
-            <button onClick={e=>deleteLocation(id)} className="">Delete Location</button>
-            <button onClick={e=>{setIsEdit(true)}} className="">Edit Location</button>
-          </div>
-       : <></>}
-  
-          <div>info: {info}</div>
-          <div>address: {address}</div> 
-        </>}
-    </div>);
-  }
-
-const locationForm =  {address: "", info: ""}
-
-function LocationForm({
-  cancelLocationForm = ()=>{} , 
-  putStoreOwnerLocation = ()=>{},
-  form = locationForm
- }){
-
-  const [ formFields, setFormFeilds ] = useState(form)
-  const areFieldsValid = () => formFields.info.trim() && formFields.address.trim() 
-
-  function handleChange(e){
-    setFormFeilds({
-      ...formFields,
-      [e.target.name]: e.target.value
-    })
-  }
-
-  function handleSubmit(e){
-    e.preventDefault();
-    console.log(formFields);
-    putStoreOwnerLocation({...formFields, icons: []}); //ignore the icons for now
-  }
-
-  return(<>
-
-     <form onSubmit={handleSubmit}>
-      <button onClick={cancelLocationForm} className="cancel_new_appointment_btn">X</button>
-      <div className="form_row">
-        address: <input name="address" value={formFields.address.trim()} className="appointment_form_input" onChange={handleChange} required /> 
-      </div>
-      <div className="form_row">
-        info:  <input name="info" value={formFields.info.trim()} className="appointment_form_input" onChange={handleChange} required />  
-      </div>
-      <input type="submit" value="Confirm" disabled={!areFieldsValid()}/>
-    </form>
-
-  </>)
-
 }
 
+function LocationPanel({ //a panel encapsulates the different states of the location card
+    location = {},
+    isLocationSelected = false,
+    selectLocation=()=>{}, 
+    putStoreOwnerLocation = ()=>{},
+    _deleteLocation=()=>{}, 
+    _editStoreOwnerLocation=()=>{},
+    startingMode = LocationPanelState.Data,
+    selectedLocationId
+}){
+
+    const { isStoreOwner } = useAuth();  
+
+    const [ mode, setMode ] = useState( startingMode )
+
+    useEffect(()=>setMode(startingMode), [isLocationSelected, selectedLocationId])
+
+    const {info, address, id, LatLng, } = location
+
+    function getUI(selectedMode){
+        
+        switch(selectedMode){
+            case LocationPanelState.AddButton:
+                return <button onClick={e=>setMode(LocationPanelState.New)}>Add New Location</button>
+            case LocationPanelState.New:
+                return <>
+                    <LocationForm
+                        cancelForm={()=>setMode(LocationPanelState.AddButton)}
+                        submitForm ={putStoreOwnerLocation}
+                        form={ locationForm  }
+                        LocationFormState = {LocationFormState.New}/>
+                </>
+            case LocationPanelState.Edit:
+               return <>
+                    <LocationForm
+                        cancelForm={()=>setMode(LocationPanelState.Data)}
+                        submitForm ={_editStoreOwnerLocation}
+                        form={ {...location} }
+                        LocationFormState = {LocationFormState.Edit}/>
+                </>
+            case LocationPanelState.Data:
+                return<>
+                    <div>info: {info}</div>
+                    <div>address: {address}</div>
+                    {isStoreOwner() && isLocationSelected ? 
+                        <div className="location_card_btns"> 
+                             <button onClick={e=>_deleteLocation(id)} className="">Delete Location</button>
+                             <button onClick={e=>{setMode(LocationPanelState.Edit)}} className="">Edit Location</button>
+                        </div> : <></>}
+                </>;
+            default: return <></>
+        }
+    }
+
+    return(
+        <div className={isLocationSelected ? "location_card location_card_selected" : "location_card" } onClick={ e=>{ selectLocation(id)}}>
+            {getUI(mode)}
+        </div>
+    );
+}
+
+function LocationForm({
+  cancelForm = ()=>{} , 
+  submitForm = ()=>{},
+  form = locationForm,
+ // LocationFormState = ""
+ }){
+
+    const [ formFields, setFormFeilds ] = useState({...form})
+    const areFieldsValid = () => formFields.info.trim() && formFields.address.trim() 
+
+    function handleChange(e){
+    setFormFeilds({
+        ...formFields,
+        [e.target.name]: e.target.value
+    })}
+
+    function handleSubmit(e){
+        e.preventDefault();
+        console.log(formFields);
+        submitForm ({...formFields, icons: []}); //ignore the icons for now
+        cancelForm()}   
+
+    return(<>
+        <form onSubmit={handleSubmit}>
+            <button onClick={cancelForm} className="cancel_new_appointment_btn">X</button>
+            <div className="form_row">
+            address: <input name="address" value={formFields.address.trim()} className="appointment_form_input" onChange={handleChange} required /> 
+            </div>
+            <div className="form_row">
+            info:  <input name="info" value={formFields.info.trim()} className="appointment_form_input" onChange={handleChange} required />  
+            </div>
+            <input type="submit" value="Confirm" disabled={!areFieldsValid()}/>
+        </form> 
+    </>)
+}
+
+
 export default LocationList
+
+  
+
+ /*
+
+
+  function LocationCard({
+    info, 
+    address,
+    isLocationSelected,
+    _deleteLocation,
+
+ }){
+
+  const { isStoreOwner } = useAuth();  
+
+  return(<>
+    <div>info: {info}</div>
+    <div>address: {address}</div>
+    {isStoreOwner() && isLocationSelected ? 
+        <div className="location_card_btns"> 
+             <button onClick={e=>_deleteLocation(id)} className="">Delete Location</button>
+             <button onClick={e=>{setMode(LocationPanelState.Edit)}} className="">Edit Location</button>
+        </div> : <></>}
+</>);
+ }
+  
+
+*/
---
+++ src/components/Map.tsx
@@ -0,0 +1,353 @@
+//AIzaSyDqveqKgLlKG9gO1NCrs-iHmSjx10TUTkE
+
+/*
+todo:
+- make the map move and pan smoothly when switching locations
+  -- right now it just jarringly moves suddenly
+
+- save the zoom levels between re-renders
+  -- save it on the map object
+
+  - make selected icons glow or something?
+  - make icons with appointments distinguishable
+
+  - add a custom loading screen?
+    -- should probabley be added to the entire screen
+
+  - look into why the right side of the map is getting pushed out
+    -- prob the flexbox css on the right container
+*/
+
+import * as React from "react";
+
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import { createCustomEqual } from "fast-equals";
+import { isLatLngLiteral } from "@googlemaps/typescript-guards";
+
+
+    const render = (status: Status) => {
+        return <h1>{status}</h1>;
+      };
+      
+      const MyMap = ( { 
+        posts, 
+        selected, 
+        handleSelectedLocation }) => {
+
+
+
+        console.log("Map start:" , posts, selected)
+
+       // const { 
+        //  data, 
+        //  selected, 
+        //  handleSelectedLocation } = props?.context
+
+       const markers = posts || [];
+
+       const [zoom, setZoom] = React.useState(4); // initial zoom
+
+      const [center, setCenter] = React.useState<google.maps.LatLngLiteral>({
+          lat: 45.91961776025469,
+          lng: -0.7844604492,
+        });
+        
+        const onMarkerClick = (iw: google.maps.InfoWindow, e: google.maps.MapMouseEvent, id: Number) => {
+
+         // console.log("onmarkerclick: ", iw, e, id)
+
+          const {lat, lng} = e.latLng.toJSON()
+
+          //console.log("lat: ", lat, " lng: ", lng)
+          setCenter({lat: lat, lng: lng})
+
+          //selectedLocationHandler(null, id)
+          handleSelectedLocation(null,id)
+          
+
+         // setZoom(4)
+        };
+      
+        const onClick = (e: google.maps.MapMouseEvent) => {
+
+          const {lat, lng} = e.latLng.toJSON()
+          //setClicks([...clicks, e.latLng!]);
+          //setCenter({lat: lat, lng: lng})
+          //setZoom(15)
+        };
+      
+        const onIdle = (m: google.maps.Map) => {
+          //console.log("onIdle");
+          setZoom(m.getZoom()!);
+          setCenter(m.getCenter()!.toJSON());
+        };
+      
+        /*
+        const form = (
+          <div
+            style={{
+              padding: "1rem",
+              flexBasis: "250px",
+              height: "100%",
+              overflow: "auto",
+            }}
+          >
+            <label htmlFor="zoom">Zoom</label>
+            <input
+              type="number"
+              id="zoom"
+              name="zoom"
+              value={zoom}
+              onChange={(event) => setZoom(Number(event.target.value))}
+            />
+            <br />
+            <label htmlFor="lat">Latitude</label>
+            <input
+              type="number"
+              id="lat"
+              name="lat"
+              value={center.lat}
+              onChange={(event) =>
+                setCenter({ ...center, lat: Number(event.target.value) })
+              }
+            />
+            <br />
+            <label htmlFor="lng">Longitude</label>
+            <input
+              type="number"
+              id="lng"
+              name="lng"
+              value={center.lng}
+              onChange={(event) =>
+                setCenter({ ...center, lng: Number(event.target.value) })
+              }
+            />
+            <h3>{clicks.length === 0 ? "Click on map to add markers" : "Clicks"}</h3>
+            {clicks.map((latLng, i) => (
+              <pre key={i}><></>{ JSON.stringify(latLng.toJSON(), null, 2)}</pre>
+            ))}
+            <button onClick={() => setClicks([])}>Clear</button>
+          </div>
+        );
+        */
+
+        return (
+          <div style={{ display: "flex", height: "100%" , width: "100%"}}>
+            <Wrapper apiKey={"AIzaSyDqveqKgLlKG9gO1NCrs-iHmSjx10TUTkE"} render={render}>
+              <Map
+                center={center}
+                //onClick={onClick}
+                //onIdle={onIdle}
+                zoom={zoom}
+                style={{ flexGrow: "1", height: "100%" }}
+              >
+                {markers.map((data, i) => (
+                  <Marker                               /* instantiate a new marker object for each entry in the list */
+                    key={data.id}
+                    id={data.id}
+                    selected={data.id === selected} 
+                    position={data.LatLng}
+                    title={"marker title"} 
+                    label={i.toString()}
+                    content={data.info}
+                    onClick={onMarkerClick}              
+                  />
+                ))}
+              </Map>
+            </Wrapper>
+
+            {/*form*/}
+          </div>
+        );
+      };
+      interface MapProps extends google.maps.MapOptions {
+        style: { [key: string]: string };
+        onClick?: (e: google.maps.MapMouseEvent) => void;
+        onIdle?: (map: google.maps.Map) => void;
+        children?: React.ReactNode;
+      }
+      
+      const Map: React.FC<MapProps> = ({
+        onClick,
+        onIdle,
+        children,
+        style,
+        ...options
+      }) => {
+        const ref = React.useRef<HTMLDivElement>(null);
+        const [map, setMap] = React.useState<google.maps.Map>();
+
+        const [openInfoWindow, setOpenInfoWindow] = React.useState<google.maps.InfoWindow>();
+        
+        /* when a markers info window is open, close the current open info window */
+        const toggleInfoWindow = (iw: google.maps.InfoWindow, m: google.maps.Marker) =>{
+
+          
+          //console.log("hello info window: ", iw, m)
+
+          /* if there exists an openInfoWindow, close it..*/
+          if(openInfoWindow){
+            openInfoWindow.close()
+          }
+
+          /* open the passed in infoWindow and set it to the component */
+          iw && iw.open({anchor: m, map: map});
+          setOpenInfoWindow(iw)    
+        }
+
+        React.useEffect(() => {
+          if (ref.current && !map) {
+            setMap(new window.google.maps.Map(ref.current, {}));
+          }
+        }, [ref, map]);
+      
+        // because React does not do deep comparisons, a custom hook is used
+        // see discussion in https://github.com/googlemaps/js-samples/issues/946
+        useDeepCompareEffectForMaps(() => {
+          if (map) {
+            map.setOptions(options);
+          }
+        }, [map, options]);
+      
+        React.useEffect(() => {
+          
+          if (map) {
+            ["click", "idle"].forEach( (eventName) => {
+              google.maps.event.clearListeners(map, eventName)
+          });
+      
+            if (onClick) {
+              //map.addListener("click", onClick);
+            }
+      
+            if (onIdle) {
+              map.addListener("idle", () => onIdle(map));
+            }
+          }
+        }, [map, onClick, onIdle]);
+      
+        return (
+          <>
+            <div ref={ref} style={style} />
+            {React.Children.map(children, (child) => {
+              if (React.isValidElement(child)) {
+                // set the map prop on the child component
+                // @ts-ignore
+                return React.cloneElement(child, { map, toggleInfoWindow });
+              }
+            })}
+          </>
+        );
+      };
+
+/*
+      interface MarkerProps extends google.maps.MarkerOptions {
+        
+        onClick?: (e: google.maps.MapMouseEvent) => void;
+      }
+  */    
+      const Marker: React.FC<google.maps.MarkerOptions> = (options) => {
+
+      const [marker, setMarker] = React.useState<google.maps.Marker>();
+
+      const [infoWindow, setInfoWindow] = React.useState<google.maps.InfoWindow>();
+
+      //.log("marker: ", options)
+
+      const { map, content, id, selected, toggleInfoWindow } = options;
+
+      //console.log("marker selected: ", selected)
+
+        React.useEffect(() => {
+
+          //console.log("marker useeffect: ", id)
+
+          if (!marker) {
+            const myMarker = new google.maps.Marker({title: options.title});
+            const infowindow = new google.maps.InfoWindow({content: content}); 
+            
+            setInfoWindow(infowindow)
+            setMarker(myMarker);
+
+            if(selected){
+              toggleInfoWindow(infoWindow, marker)
+            }
+            
+          }
+      
+          // remove marker from map on unmount
+          return () => {
+            if (marker) {
+              //console.log("marker removal")
+              marker.setMap(null);
+              
+            }
+          };
+        }, [marker, infoWindow]);
+
+      
+        React.useEffect(
+          () => {
+          if (marker) {
+
+            ["click"].forEach( (eventName) => {
+                google.maps.event.clearListeners(marker, eventName)
+            });
+
+            marker.setOptions(options);
+
+            marker.addListener("click", (e) => {
+              options.onClick(infoWindow, e, id)
+              toggleInfoWindow(infoWindow, marker)
+            });
+
+            /* when a marker is selected; either by clicking on it on the map or clicking the cooresponding list widget */
+            if(selected){
+              toggleInfoWindow(infoWindow, marker) // close the current info window (if open) and open this one
+              map.setCenter(marker.getPosition()) // and center the map on the marker
+            }
+          }      
+        }, [marker, options, infoWindow]);
+      
+        return null;
+      };
+
+      const deepCompareEqualsForMaps = createCustomEqual(
+        (deepEqual) => (a: any, b: any) => {
+
+         // console.log("188")
+
+          if (
+            isLatLngLiteral(a) ||
+            a instanceof google.maps.LatLng ||
+            isLatLngLiteral(b) ||
+            b instanceof google.maps.LatLng
+          ) {
+            return new google.maps.LatLng(a).equals(new google.maps.LatLng(b));
+          }
+      
+          // TODO extend to other types
+      
+          // use fast-equals for other objects
+          return deepEqual(a, b);
+        }
+      );
+      
+      function useDeepCompareMemoize(value: any) {
+        const ref = React.useRef();
+      
+        if (!deepCompareEqualsForMaps(value, ref.current)) {
+          ref.current = value;
+        }
+      
+        return ref.current;
+      }
+      
+      function useDeepCompareEffectForMaps(
+        callback: React.EffectCallback,
+        dependencies: any[]
+      ) {
+        React.useEffect(callback, dependencies.map(useDeepCompareMemoize));
+      }
+
+export default MyMap;
