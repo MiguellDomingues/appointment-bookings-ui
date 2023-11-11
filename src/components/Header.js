8abd7e823a57f92b5abe4737ac6ef8d2e7b917d6
Miguel Domingues <42451135+MiguellDomingues@users.noreply.github.com>
2023-11-11 10:32:02 -0800

    moved header cmp to own file

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import { useAuth } from '../AuthProvider'
+
+function Header({
+refetchLocations,
+refetching,
+handleSetMapPage,
+handleSetCalendarPage
+}){
+
+const { token, loadingUser,  isUser, isStoreOwner, isGuest } = useAuth();
+
+function getLinks(){
+    if(isStoreOwner()){
+    return <>
+        <span onClick={handleSetMapPage}>Locations</span>
+        <span onClick={handleSetCalendarPage}>Appointments</span>
+    </>
+    }else{
+    return <><span onClick={handleSetMapPage}>Locations</span></>
+    }
+}
+
+return( 
+    <div className="header">
+
+        {getLinks()}
+
+        <span>
+        {loadingUser ? 
+            <>Athenticating....</> : 
+            <>Welcome {token && token?.username ? <>{token.username}</> : <>Guest</> }    
+        </>}
+        </span>
+        
+        <span> <button disabled={refetching} onClick={e=>refetchLocations()}>Refresh</button> </span>
+    </div>);
+}
+
+export default Header;
